dd684125b231bb8c5efb9f833801495ba1764d5a
YangHongwei <403236160@qq.com>
2023-10-30 16:00:38 +0800

    feat: 使用springboot创建项目

---
+++ src/main/java/com/sxqibo/spring3admin/Spring3adminApplication.java
@@ -0,0 +1,13 @@
+package com.sxqibo.spring3admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3adminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3adminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxqibo/spring3admin/Spring3adminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxqibo.spring3admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3adminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
