c79994bbd18b1ee525b3c6ecb2f03ec54ef8809f
hongwei <403236160@qq.com>
2023-11-05 19:19:36 +0800

    feat: 开发hello world接口

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# spring3admin
+# Spring3 Admin
+
+## 环境要求
+* JAVA17 
+* JDK17
+* Maven3
+* Spring Boot 3.1.5
--- src/main/java/com/sxqibo/spring3admin/config/Spring3adminApplication.java
+++ src/main/java/com/sxqibo/spring3admin/config/Spring3adminApplication.java
@@ -1,12 +1,14 @@
-package com.sxqibo.spring3admin;
+package com.sxqibo.spring3admin.config;
 
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.sxqibo")
 @SpringBootApplication
 public class Spring3adminApplication {
 
