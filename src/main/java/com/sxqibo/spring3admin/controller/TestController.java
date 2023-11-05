601b0d5cb1509fcc6b48cee36a59355c102d50f1
hongwei <403236160@qq.com>
2023-11-05 19:44:41 +0800

    feat: 使用HTTP Client测试接口

--- src/main/java/com/sxqibo/spring3admin/controller/TestController.java
+++ src/main/java/com/sxqibo/spring3admin/controller/TestController.java
@@ -2,14 +2,18 @@ package com.sxqibo.spring3admin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping ("/hello")
+    @GetMapping ("/api/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/api/hello/post")
+    public String helloPost(String name) {
+        return "Hello World!Post," + name;
     }
 }
