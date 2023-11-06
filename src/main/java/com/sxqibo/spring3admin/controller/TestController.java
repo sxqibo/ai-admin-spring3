3e6692342a921e698990de95ff9d2bba863e0898
YangHongwei <403236160@qq.com>
2023-11-06 16:20:40 +0800

    feat: 默认值的使用

--- src/main/java/com/sxqibo/spring3admin/controller/TestController.java
+++ src/main/java/com/sxqibo/spring3admin/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.sxqibo.spring3admin.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
     @GetMapping ("/api/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/api/hello/post")
