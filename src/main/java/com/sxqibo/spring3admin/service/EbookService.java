69d4e95143df5d8edfe9fdeebe5ebe8c3184e19a
YangHongwei <403236160@qq.com>
2023-11-20 17:54:38 +0800

    feat: 查询的优化

--- src/main/java/com/sxqibo/spring3admin/service/EbookService.java
+++ src/main/java/com/sxqibo/spring3admin/service/EbookService.java
@@ -1,6 +1,5 @@
 package com.sxqibo.spring3admin.service;
 
-import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.sxqibo.spring3admin.domain.Ebook;
 import com.sxqibo.spring3admin.domain.EbookExample;
 import com.sxqibo.spring3admin.mapper.EbookMapper;
@@ -8,10 +7,9 @@ import com.sxqibo.spring3admin.req.EbookReq;
 import com.sxqibo.spring3admin.resp.EbookResp;
 import com.sxqibo.spring3admin.util.CopyUtil;
 import jakarta.annotation.Resource;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,7 +22,9 @@ public class EbookService {
         // 查询
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         // 列表复制
