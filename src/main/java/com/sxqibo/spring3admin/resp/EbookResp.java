0eb423094c9cb3a7af69217198ed4ce9f4efc763
YangHongwei <403236160@qq.com>
2023-11-25 17:22:58 +0800

    feat: 传参的更改，尤其是对于下划线的字段的操作

--- src/main/java/com/sxqibo/spring3admin/resp/EbookResp.java
+++ src/main/java/com/sxqibo/spring3admin/resp/EbookResp.java
@@ -7,6 +7,18 @@ public class EbookResp {
 
     private String description;
 
+    private Integer category1_id;
+
+    private Integer category2_id;
+
+    private String cover;
+
+    private Integer doc_count;
+
+    private Integer vote_count;
+
+
+
     public Integer getId() {
         return id;
     }
@@ -31,6 +43,48 @@ public class EbookResp {
         this.description = description;
     }
 
+
+    public Integer getCategory1_id() {
+        return category1_id;
+    }
+
+    public void setCategory1Id(Integer category1_id) {
+        this.category1_id = category1_id;
+    }
+
+    public Integer getCategory2_id() {
+        return category2_id;
+    }
+
+    public void setCategory2Id(Integer category2_id) {
+        this.category2_id = category2_id;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Integer getDoc_count() {
+        return doc_count;
+    }
+
+    public void setDocCount(Integer doc_count) {
+        this.doc_count = doc_count;
+    }
+
+    public Integer getVote_count() {
+        return vote_count;
+    }
+
+    public void setVoteCount(Integer vote_count) {
+        this.vote_count = vote_count;
+    }
+
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -40,6 +94,9 @@ public class EbookResp {
         sb.append(", id=").append(id);
         sb.append(", name=").append(name);
         sb.append(", description=").append(description);
+        sb.append(", cover=").append(description);
+        sb.append(", doc_count=").append(description);
+        sb.append(", vote_count=").append(description);
         sb.append("]");
         return sb.toString();
     }
